a5a769144f6a57f2347ca765f5f18673717568f9
Arjan van Maren <Arjanvanmaren@gmail.com>
2020-02-16 11:30:21 +0100

    Updates

---
+++ precache-manifest.f26697129d487a26c87bd279826ac22e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61a07362a320d3865d43",
+    "url": "/me/static/css/main.fc78bf77.chunk.css"
+  },
+  {
+    "revision": "61a07362a320d3865d43",
+    "url": "/me/static/js/main.61a07362.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "60ff19646cd5e02e49b7a6839fec9a0d",
+    "url": "/me/index.html"
+  }
+];
